7950637b54281575aa5d5d0b45b0e74c5e335008
s0ph1e <sonya1912@gmail.com>
2014-11-23 23:46:03 +0200

    Add some utils tests

--- test/utils-test.js
+++ test/utils-test.js
@@ -1,20 +1,60 @@
 var utils = require('../lib/utils/utils');
 var should = require('should')
   describe('Common utils', function(){
-    describe('#isUrl()', function(){
+    describe('#isUrl(url)', function(){
       it('should return true if url starts with "http[s]://"', function(){
         utils.isUrl('http://google.com').should.be.true;
-        utils.isUrl('http://google.com').should.be.true;
-      })
+        utils.isUrl('https://github.com').should.be.true;
+      });
       it('should return true if url starts with "//"', function(){
         utils.isUrl('//www.youtube.com').should.be.true;
-      })
+      });
       it('should return false if url starts neither with "http[s]://" nor "//"', function(){
         utils.isUrl('http//www.youtube.com').should.be.false;
         utils.isUrl('http:/www.youtube.com').should.be.false;
         utils.isUrl('htt://www.youtube.com').should.be.false;
         utils.isUrl('://www.youtube.com').should.be.false;
         utils.isUrl('www.youtube.com').should.be.false;
-      })
-    })
+      });
+    });
+
+    describe('#getUrl(url, path)', function(){
+      it('should return url + path if path is not url', function(){
+        utils.getUrl('http://google.com', '/path').should.be.equal('http://google.com/path');
+        utils.getUrl('http://google.com/qwe/qwe/qwe', '/path').should.be.equal('http://google.com/path');
+        utils.getUrl('http://google.com?kjrdrgek=dmskl', '/path').should.be.equal('http://google.com/path');
+      });
+      it('should return path if it is url', function(){
+        utils.getUrl('http://google.com', 'http://my.site.com/').should.be.equal('http://my.site.com/');
+        utils.getUrl('http://google.com/qwe/qwe/qwe', '//my.site.com').should.be.equal('http://my.site.com/');
+      });
+    });
+
+    describe('#getUnixPath(path)', function(){
+      it('should convert to unix format for windows', function(){
+        utils.getUnixPath('D:\\Projects\\node-website-scraper').should.be.equal('D:/Projects/node-website-scraper');
+      });
+      it('should return unconverted path for unix', function(){
+        utils.getUnixPath('/home/sophia/projects/node-website-scraper').should.be.equal('/home/sophia/projects/node-website-scraper');
+      });
+    });
+
+    describe('#trimFilename(filename)', function(){
+      it('should trim all after first ? or #', function(){
+        utils.trimFilename('index.html?12').should.be.equal('index.html');
+        utils.trimFilename('index.html#t?12').should.be.equal('index.html');
+        utils.trimFilename('index.html?12#t').should.be.equal('index.html');
+        utils.trimFilename('?12_jdlsk').should.be.empty;
+        utils.trimFilename('#index.html').should.be.empty;
+      });
+      it('should return unconvetred filename if there are no ?,#', function(){
+        utils.trimFilename('index.html').should.be.equal('index.html');
+      });
+    });
+
+    describe('#makeRequest(url)', function(){
+//      it('should be a promise', function(){
+//
+//      });
+    });
   });
