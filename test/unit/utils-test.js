1d19820f51ec1da4b47df0a90bbe0e0daa64c7ff
s0ph1e <sonya1912@gmail.com>
2015-08-05 22:44:26 +0300

    Revert "Remove excess code"
    This reverts commit b3e4bf7c1ed787f02ec61a5d95aebe1e3a985cf5.

--- test/unit/utils-test.js
+++ test/unit/utils-test.js
@@ -2,6 +2,23 @@ require('should');
 var utils = require('../../lib/utils');
 
 describe('Common utils', function () {
+	describe('#isUrl(url)', function () {
+		it('should return true if url starts with "http[s]://"', function () {
+			utils.isUrl('http://google.com').should.be.true();
+			utils.isUrl('https://github.com').should.be.true();
+		});
+		it('should return true if url starts with "//"', function () {
+			utils.isUrl('//www.youtube.com').should.be.true();
+		});
+		it('should return false if url starts neither with "http[s]://" nor "//"', function () {
+			utils.isUrl('http//www.youtube.com').should.be.false();
+			utils.isUrl('http:/www.youtube.com').should.be.false();
+			utils.isUrl('htt://www.youtube.com').should.be.false();
+			utils.isUrl('://www.youtube.com').should.be.false();
+			utils.isUrl('www.youtube.com').should.be.false();
+		});
+	});
+
 	describe('#getUrl(url, path)', function () {
 		it('should return url + path if path is not url', function () {
 			utils.getUrl('http://google.com', '/path').should.be.equal('http://google.com/path');
