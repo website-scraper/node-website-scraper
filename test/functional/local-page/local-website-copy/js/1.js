d39c3a07ce8df0af9c03f12b1129aba3dfa1fd5e
mmicu <marco.micucci@studenti.unicam.it>
2015-11-05 23:11:12 +0100

    Added the possibility to parse local web pages

---
+++ test/functional/local-page/local-website-copy/js/1.js
@@ -0,0 +1,14 @@
+function foo () {
+  alert ('foo');
+}
+
+function foo_2 () {
+  return { x: 'asd', y: 'lol' };
+}
+
+
+$(document).ready (function () {
+  $('#link_1').click (function () {
+    alert ('click');
+  });
+});
