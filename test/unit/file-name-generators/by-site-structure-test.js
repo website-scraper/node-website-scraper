c29a809ab655ac1d0fc458708d270e2076890042
Mark Lagendijk <mark@lagendijk.info>
2016-04-20 16:47:59 +0100

    Finished unit test implementation.

---
+++ test/unit/file-name-generators/by-site-structure-test.js
@@ -0,0 +1,45 @@
+require('should');
+var Resource = require('../../../lib/resource');
+var bySiteStructureFilenameGenerator = require('../../../lib/filename-generators/by-site-structure');
+
+var options = { defaultFilename: 'index.html' };
+
+describe('byTypeFilenameGenerator', function() {
+	it('should return the normalized absolute path of the resource url', function(){
+		var r1 = new Resource('http://example.com/some/path/a.png');
+		bySiteStructureFilenameGenerator(r1, options).should.equal('/some/path/a.png');
+
+		var r2 = new Resource('http://example.com/a.png');
+		bySiteStructureFilenameGenerator(r2, options).should.equal('/a.png');
+
+		var r3 = new Resource('http://example.com/some/path/../images/a.png');
+		bySiteStructureFilenameGenerator(r3, options).should.equal('/some/images/a.png');
+	});
+
+	it('should add the defaultFilename to the path, for html resources without extension', function(){
+		var isHtmlMock = function(){
+			return true;
+		};
+
+		var r1 = new Resource('http://example.com/some/path/');
+		r1.isHtml = isHtmlMock;
+		bySiteStructureFilenameGenerator(r1, options).should.equal('/some/path/index.html');
+
+		var r2 = new Resource('http://example.com/some/path');
+		r2.isHtml = isHtmlMock;
+		bySiteStructureFilenameGenerator(r2, options).should.equal('/some/path/index.html');
+
+		var r3 = new Resource('http://example.com');
+		r3.isHtml = isHtmlMock;
+		bySiteStructureFilenameGenerator(r3, options).should.equal('/index.html');
+
+		var r4 = new Resource('');
+		r4.isHtml = isHtmlMock;
+		bySiteStructureFilenameGenerator(r4, options).should.equal('/index.html');
+	});
+
+	it('should normalize to safe absolute paths, without ..', function(){
+		var r = new Resource('http://example.com/some/path/../../../../images/a.png');
+		bySiteStructureFilenameGenerator(r, options).should.equal('/images/a.png');
+	})
+});
--- test/unit/file-name-generators/by-type-test.js
+++ test/unit/file-name-generators/by-type-test.js
@@ -1,106 +1,11 @@
-/*
-describe('#getOccupiedFilenames', function() {
-    it('should return empty array if nothing loaded and no subdirectories', function(done) {
-        var s = new Scraper({
-            urls: 'http://example.com',
-            directory: testDirname,
-            subdirectories: null
-        });
+require('should');
+var Scraper = require('../../../lib/scraper');
+var Resource = require('../../../lib/resource');
+var byTypeFilenameGenerator = require('../../../lib/filename-generators/by-type');
 
-        s.prepare().then(function() {
-            var occupied = s.getOccupiedFilenames();
-            occupied.should.be.instanceOf(Array);
-            occupied.should.be.empty();
-            done();
-        }).catch(done);
-    });
+var testDirname = __dirname + '/.scraper-test';
 
-    it('should contain all filenames for loaded resources', function(done) {
-        var s = new Scraper({
-            urls: 'http://example.com',
-            directory: testDirname,
-            subdirectories: null
-        });
-
-        s.prepare().then(function() {
-            var a = new Resource('http://first-resource.com', 'first.html');
-            var b = new Resource('http://second-resource.com', 'second.html');
-            var c = new Resource('http://third-resource.com', 'third.html');
-            s.addLoadedResource(a);
-            s.addLoadedResource(b);
-            s.addLoadedResource(c);
-
-            var occupied = s.getOccupiedFilenames();
-            occupied.should.be.instanceOf(Array);
-            occupied.should.containEql('first.html');
-            occupied.should.containEql('second.html');
-            occupied.should.containEql('third.html');
-            done();
-        }).catch(done);
-    });
-
-    it('should contain all subdirectories names', function(done) {
-        var s = new Scraper({
-            urls: 'http://example.com',
-            directory: testDirname,
-            subdirectories: [
-                { directory: 'img', extensions: ['.jpg', '.png', '.svg'] },
-                { directory: 'js', extensions: ['.js'] },
-                { directory: 'css', extensions: ['.css'] }
-            ]
-        });
-
-        s.prepare().then(function() {
-            var occupied = s.getOccupiedFilenames();
-            occupied.should.be.instanceOf(Array);
-            occupied.should.containEql('img');
-            occupied.should.containEql('js');
-            occupied.should.containEql('css');
-            done();
-        }).catch(done);
-    });
-});
-
-describe('#getDirectoryByExtension', function() {
-    it('should return correct subdirectory', function(done) {
-        var s = new Scraper({
-            urls: 'http://example.com',
-            directory: testDirname,
-            subdirectories: [
-                { directory: 'img', extensions: ['.jpg', '.png', '.svg'] },
-                { directory: 'js', extensions: ['.js'] }
-            ]
-        });
-
-        s.prepare().then(function() {
-            s.getDirectoryByExtension('.jpg').should.be.eql('img');
-            s.getDirectoryByExtension('.png').should.be.eql('img');
-            s.getDirectoryByExtension('.svg').should.be.eql('img');
-            s.getDirectoryByExtension('.js').should.be.eql('js');
-            done();
-        }).catch(done);
-    });
-
-    it('should return empty string if no subdirectory was found', function(done) {
-        var s = new Scraper({
-            urls: 'http://example.com',
-            directory: testDirname,
-            subdirectories: [
-                { directory: 'img', extensions: ['.jpg', '.png', '.svg'] },
-                { directory: 'js', extensions: ['.js'] }
-            ]
-        });
-
-        s.prepare().then(function() {
-            s.getDirectoryByExtension('.gif').should.be.eql('');
-            s.getDirectoryByExtension('.html').should.be.eql('');
-            s.getDirectoryByExtension('.css').should.be.eql('');
-            done();
-        }).catch(done);
-    });
-});
-
-describe('#generateFilename', function() {
+describe('byTypeFilenameGenerator', function() {
     it('should return resource filename', function(done) {
         var s = new Scraper({
             urls: 'http://example.com',
@@ -110,7 +15,7 @@ describe('#generateFilename', function() {
 
         s.prepare().then(function() {
             var r = new Resource('http://example.com/a.png', 'b.png');
-            var filename = s.generateFilename(r);
+            var filename = byTypeFilenameGenerator(r, s.options, s.loadedResources);
             filename.should.be.eql('b.png');
             done();
         }).catch(done);
@@ -125,7 +30,7 @@ describe('#generateFilename', function() {
 
         s.prepare().then(function() {
             var r = new Resource('http://example.com/a.png', '');
-            var filename = s.generateFilename(r);
+			var filename = byTypeFilenameGenerator(r, s.options, s.loadedResources);
             filename.should.be.eql('a.png');
             done();
         }).catch(done);
@@ -142,7 +47,7 @@ describe('#generateFilename', function() {
 
         s.prepare().then(function() {
             var r = new Resource('http://example.com/a.png');
-            var filename = s.generateFilename(r);
+			var filename = byTypeFilenameGenerator(r, s.options, s.loadedResources);
             filename.should.be.eql('img/a.png');
             done();
         }).catch(done);
@@ -159,12 +64,12 @@ describe('#generateFilename', function() {
             var r1 = new Resource('http://first-example.com/a.png');
             var r2 = new Resource('http://second-example.com/a.png');
 
-            var f1 = s.generateFilename(r1);
+            var f1 = byTypeFilenameGenerator(r1, s.options, s.loadedResources);
             f1.should.be.eql('a.png');
             r1.setFilename(f1);
             s.addLoadedResource(r1);
 
-            var f2 = s.generateFilename(r2);
+            var f2 = byTypeFilenameGenerator(r2, s.options, s.loadedResources);
             f2.should.be.not.eql('a.png');
 
             done();
@@ -184,23 +89,23 @@ describe('#generateFilename', function() {
             var r3 = new Resource('http://third-example.com/a.png');
             var r4 = new Resource('http://fourth-example.com/a.png');
 
-            var f1 = s.generateFilename(r1);
+			var f1 = byTypeFilenameGenerator(r1, s.options, s.loadedResources);
             f1.should.be.eql('a.png');
             r1.setFilename(f1);
             s.addLoadedResource(r1);
 
-            var f2 = s.generateFilename(r2);
+			var f2 = byTypeFilenameGenerator(r2, s.options, s.loadedResources);
             f2.should.be.not.eql(r1.getFilename());
             r2.setFilename(f2);
             s.addLoadedResource(r2);
 
-            var f3 = s.generateFilename(r3);
+			var f3 = byTypeFilenameGenerator(r3, s.options, s.loadedResources);
             f3.should.be.not.eql(r1.getFilename());
             f3.should.be.not.eql(r2.getFilename());
             r3.setFilename(f3);
             s.addLoadedResource(r3);
 
-            var f4 = s.generateFilename(r4);
+			var f4 = byTypeFilenameGenerator(r4, s.options, s.loadedResources);
             f4.should.be.not.eql(r1.getFilename());
             f4.should.be.not.eql(r2.getFilename());
             f4.should.be.not.eql(r3.getFilename());
@@ -209,4 +114,3 @@ describe('#generateFilename', function() {
         }).catch(done);
     });
 });
-*/
