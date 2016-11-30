ed3994ad0782559be0ae175acbefdef5c1cdd037
s0ph1e <sonya1912@gmail.com>
2016-11-30 20:30:13 +0200

    Add test

--- test/unit/resource-handler/html.test.js
+++ test/unit/resource-handler/html.test.js
@@ -4,6 +4,8 @@ var sinon = require('sinon');
 require('sinon-as-promised')(Promise);
 var Resource = require('../../../lib/resource');
 var loadHtml = require('../../../lib/resource-handler/html');
+var HtmlImgSrcsetTag = require('../../../lib/resource-handler/path-containers/html-img-srcset-tag');
+var HtmlCommonTag = require('../../../lib/resource-handler/path-containers/html-common-tag');
 
 describe('ResourceHandler: Html', function () {
 	describe('<base> tag', function () {
@@ -154,6 +156,37 @@ describe('ResourceHandler: Html', function () {
 
 		return loadHtml(context, resource).then(function() {
 			context.handleChildrenResources.called.should.be.eql(false);
+		});
+	});
+
+	it('should use correct path containers based on tag', function() {
+		var context = {
+			options: {
+				sources: [
+					{ selector: 'img', attr: 'src' },
+					{ selector: 'img', attr: 'srcset' }
+				]
+			},
+			handleChildrenResources: sinon.stub().resolves()
+		};
+
+		var html = `
+			<html lang="en">
+			<head></head>
+			<body>
+				<img src="a.png">
+				<img srcset="b.png">
+			</body>
+			</html>\
+		`;
+
+		var resource = new Resource('http://example.com', 'index.html');
+		resource.setText(html);
+
+		return loadHtml(context, resource).then(function() {
+			context.handleChildrenResources.calledTwice.should.be.eql(true);
+			context.handleChildrenResources.args[0][0].should.be.instanceOf(HtmlCommonTag);
+			context.handleChildrenResources.args[1][0].should.be.instanceOf(HtmlImgSrcsetTag);
 		});
 	});
 });
