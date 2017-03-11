65c45d26bf5f067fa57cc95f4b1186a0f585a8e5
Sophia Antipenko <sonya1912@gmail.com>
2017-03-11 14:14:49 +0200

    Export default options (#194)

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ var Scraper = require('./lib/scraper.js');
 module.exports = function scrape (options, callback) {
 	return new Scraper(options).scrape(callback);
 };
+
+module.exports.defaults = Scraper.defaults;
