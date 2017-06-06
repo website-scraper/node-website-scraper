b36392e10a558f796992599503878f381352d1a3
s0ph1e <sophia@antipenko.pp.ua>
2017-06-06 22:40:35 +0300

    Add use strict

--- test/unit/resource-handler/css.test.js
+++ test/unit/resource-handler/css.test.js
@@ -1,16 +1,18 @@
+'use strict';
+
 const should = require('should');
 const sinon = require('sinon');
 const Resource = require('../../../lib/resource');
 const CssResourceHandler = require('../../../lib/resource-handler/css');
 
-describe('ResourceHandler: Css', function () {
+describe('ResourceHandler: Css', () => {
 	it('should call downloadChildrenResources and set returned text to resource', () => {
 		const downloadChildrenPaths = sinon.stub().resolves('updated text');
 
 		const originalResource = new Resource('http://example.com');
 		const cssHandler = new CssResourceHandler({}, {downloadChildrenPaths});
 
-		return cssHandler.handle(originalResource).then(function (updatedResource) {
+		return cssHandler.handle(originalResource).then((updatedResource) => {
 			should(updatedResource).be.equal(originalResource);
 			should(updatedResource.getText()).be.eql('updated text');
 		});
