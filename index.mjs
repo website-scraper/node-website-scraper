e0a1966dd4ec48fa69f9a7d65d445a6f7eff1b6f
s0ph1e <sophia@antipenko.pp.ua>
2021-05-05 20:26:00 +0200

    Fix eslint

--- index.mjs
+++ index.mjs
@@ -1,15 +1,5 @@
-import Promise from 'bluebird';
 import Scraper from './lib/scraper.js';
 
 export default (options) => {
-	return Promise.try(() => {
-		return new Scraper(options).scrape();
-	});
-}
-// module.exports = (options, callback) => {
-// 	return Promise.try(() => {
-// 		return new Scraper(options).scrape(callback);
-// 	});
-// };
-
-// module.exports.defaults = Scraper.defaults;
+	return new Scraper(options).scrape();
+};
